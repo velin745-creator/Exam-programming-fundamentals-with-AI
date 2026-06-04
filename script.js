0e60ffc14e68548c982c54bd25aa36ae8d692c06
velin745-creator <velin745@gmail.com>
2026-06-04 12:55:06 +0300

    Updated graph

--- script.js
+++ script.js
@@ -165,14 +165,19 @@ document.addEventListener('DOMContentLoaded', () => {
             
             // Hover Events for Tooltip
             row.addEventListener('mousemove', (e) => {
-                tooltip.style.opacity = '1';
-                tooltip.style.left = e.pageX + 15 + 'px';
-                tooltip.style.top = e.pageY - 20 + 'px';
-                tooltip.innerHTML = `${monthWord} #${i}\n${prospectsWord}: ${mProspects}\n${leadsWord}: ${mLeads}\n${customersWord}: ${mCustomers}`;
+                tooltip.classList.add('show');
+                tooltip.style.left = e.pageX + 20 + 'px';
+                tooltip.style.top = e.pageY - 40 + 'px';
+                tooltip.innerHTML = `
+                    <div class="tooltip-title">${monthWord} #${i}</div>
+                    <div class="tooltip-row"><div class="tooltip-dot dot-prospects"></div>${prospectsWord}: ${mProspects}</div>
+                    <div class="tooltip-row"><div class="tooltip-dot dot-leads"></div>${leadsWord}: ${mLeads}</div>
+                    <div class="tooltip-row"><div class="tooltip-dot dot-customers"></div>${customersWord}: ${mCustomers}</div>
+                `;
             });
             
             row.addEventListener('mouseleave', () => {
-                tooltip.style.opacity = '0';
+                tooltip.classList.remove('show');
             });
             
             row.appendChild(barProspects);
